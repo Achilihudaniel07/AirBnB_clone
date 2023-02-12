e2220eb3be1b8f6eb2e92bccabbb170cfa118cfe
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-02-12 19:35:48 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,11 @@
-airbnb
+#!/usr/bin/python3
+""" User Class """
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ User class that inherits BaseModel """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
