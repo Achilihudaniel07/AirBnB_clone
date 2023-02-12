9499c0408791ace32417196922cd1feae537ae2e
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-02-12 19:34:04 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,8 @@
-airbnb
+#!/usr/bin/python3
+""" Class Amenity """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class that inherits BaseModel"""
+    name = ""
